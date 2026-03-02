aa3428e952f4292b1904bd945b0e926c40c9a84e
ags <95411351+angeonseok@users.noreply.github.com>
2026-03-03 00:41:00 +0900

    1

--- 백준/30.투 포인터/1806.부분합.py
+++ 백준/30.투 포인터/1806.부분합.py
@@ -9,16 +9,30 @@
 """
 
 import sys
-# input = sys.stdin.readline
-sys.stdin = open('input.txt')
+input = sys.stdin.readline
 
 n,s = map(int, input().split())
 arr = list(map(int, input().split()))
 
-i, j = 0, 1
+#투포인터
+r, l = 0, 0
+sum_num = 0
 
-cnt = 2
-l = 0
+#최솟값을 찾아보자
+ans = 9876543210
 
-while i < j:
-    
+while r < n:
+    sum_num += arr[r]
+    r += 1
+
+    #만약 구간합이 원하는 값을 넘었다면
+    while sum_num >= s:
+        ans = min(r - l, ans)   #최소길이를 찾을 때 까지
+        sum_num -= arr[l]       #l값을 늘려
+        l+= 1
+
+#합을 만족하는 수가 없다면 0을 출력
+if ans == 9876543210 :
+    print(0)
+else:
+    print(ans)
