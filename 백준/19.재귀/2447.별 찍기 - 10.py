9d042e92ac2cf154f60e86fb08dfbb862078c2cf
angeonseok <dudwltjs@gmail.com>
2026-03-03 15:51:22 +0900

    1

--- 백준/19.재귀/2447.별 찍기 - 10.py
+++ 백준/19.재귀/2447.별 찍기 - 10.py
@@ -17,4 +17,29 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = ['*' * n for _ in range(n)]
+
+#공백만들기
+arr = [[" "] * n for _ in range(n)]
+
+#분할정복
+def star(arr, r, c, n):
+
+    #더 못쪼개면 별찍어
+    if n == 1:
+        arr[r][c] = "*"
+        return
+    
+    #전체를 9등분하겠다
+    n = n // 3
+    for i in range(3):
+        for j in range(3):
+
+            #9등분 기준 중앙은 공백으로 둬야지
+            if i == 1 and j == 1:
+                continue
+            else:
+                star(arr, r + i*n, c+ j*n, n)
+
+star(arr, 0, 0, n)
+for row in arr:
+    print("".join(row))
